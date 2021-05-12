837625ffdcfcbbd9de2286b09f2a4f36d1c74b69
triettruong1 <80166945+triettruong1@users.noreply.github.com>
2021-05-12 22:07:48 +0700

    Create CSDBookList.java

---
+++ CSDBookList.java
@@ -0,0 +1,18 @@
+package csd.book.list;
+
+/*Basic Guidelines
+-Be able to add new book
+-Find book by characters
+-Update price
+-Exit
+*/
+
+
+public class CSDBookList {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menuLoop();
+    }
+
+}
